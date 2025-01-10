eb3523b7c5485c82b18756f0b4bd62306096698f
katspaugh <381895+katspaugh@users.noreply.github.com>
2025-01-10 10:22:52 +0100

    Standalone mini-frame

--- custom.el
+++ custom.el
@@ -3,17 +3,24 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(bufler-filter-buffer-name-regexps
+   '("\\*Compile-Log\\*" "\\*Disabled Command\\*" "\\*Org [^z-a]+Output\\*" "\\*xref\\*" "\\*"))
  '(company-idle-delay nil)
  '(company-tooltip-idle-delay nil)
  '(consult-preview-key nil)
+ '(custom-enabled-themes nil)
+ '(custom-safe-themes
+   '("f1c8202c772d1de83eda4765fe21429a528a4fb350a28394d3705fe9678ed1f9" default))
  '(desktop-buffers-not-to-save ".")
  '(desktop-files-not-to-save ".")
+ '(global-tree-sitter-mode t)
  '(indent-tabs-mode nil)
  '(ivy-count-format "")
  '(mini-frame-advice-functions '(read-from-minibuffer read-string))
- '(mini-frame-show-parameters '((top . 0.2) (left . 0.5) (width . 0.9) (height . 15)))
+ '(mini-frame-show-parameters '((top . 0.4) (left . 0.5) (width . 0.6) (height . 15)))
+ '(mini-frame-standalone t)
  '(package-selected-packages
-   '(js2-mode mini-frame f consult lsp-ui lsp-mode diff-hl tide flycheck prettier-js company counsel orderless vertico editorconfig dash s rainbow-mode project-explorer rainbow-delimiters yaml-mode markdown-mode))
+   '(acme-theme treemacs bufler ibuffer-sidebar tabbar tree-sitter js2-mode mini-frame f consult lsp-ui lsp-mode diff-hl tide flycheck prettier-js company counsel orderless vertico editorconfig dash s rainbow-mode project-explorer rainbow-delimiters yaml-mode markdown-mode))
  '(standard-indent 2)
  '(vertico-count-format nil)
  '(warning-minimum-level :error))
